b3a0bdb96e382fabe5e6e4c552ff248c9e83276e
NyeintNyeint San <nyeintnyeintsan@192.168.1.2>
2023-02-04 22:51:21 +0630

    first commit

--- lib/ui/colors.dart
+++ lib/ui/colors.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 class ColorsPage extends StatefulWidget {
 
